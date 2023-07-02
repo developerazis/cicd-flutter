b0446e4bbc26b5ed19f69f9f819ee3bea56cca1c
developerazis <34157701+developerazis@users.noreply.github.com>
2023-07-02 22:20:25 +0700

    add unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,25 +6,33 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:cicd_flutter/main.dart';
 
-void main() {
+void main() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  // await DotEnv().load();
+
+  setUp(() async {
+    await dotenv.load(fileName: 'assets/.config_env');
+  });
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('5'), findsOneWidget);
+    expect(find.text('6'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('5'), findsNothing);
+    expect(find.text('6'), findsOneWidget);
   });
 }
